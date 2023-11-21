a1e2d1bd6cc9fba44197bbdc3f5b3fbd60585ab5
GoGradually <chkun3109@gmail.com>
2023-11-21 20:29:32 +0900

    231121

---
+++ infiniteNumCal.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "infiniteNumberNode.h"
+
+int main() {
+    char ch;
+    Node *head, *dot, *tail;
+
+    while ((ch = getchar() != '\n')) {
+        Node *temp = (Node *)malloc(sizeof(Node));
+    }
+}
---
+++ infiniteNumberNode.h
@@ -0,0 +1,6 @@
+typedef struct __Node {
+    int number;
+    ll pos;
+    __Node* next;
+    __Node* prev;
+} Node;
